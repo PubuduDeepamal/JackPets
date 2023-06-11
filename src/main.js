db5a8b8c5b4d2967f98f1da337ead84e35dc288a
PubuduDeepamal <pubudu@lpw.lk>
2023-06-11 05:52:40 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+import './assets/main.css'
+import './index.css' //Tailwind-new
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+import 'bootstrap/dist/js/bootstrap.js'
